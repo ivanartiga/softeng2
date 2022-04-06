924d00b1569a42ad13794165737eb78b98c0dea1
ivanartiga <94870300+ivanartiga@users.noreply.github.com>
2022-04-06 13:11:28 +0800

    Create tanodservices.dart

---
+++ screens/subpages/tanodservices.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
+
+class Tanodservices extends StatefulWidget {
+  const Tanodservices({Key? key}) : super(key: key);
+
+  @override
+  State<Tanodservices> createState() => _TanodservicesState();
+}
+
+class _TanodservicesState extends State<Tanodservices> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("TANOD SERVICES"),);
+  }
+}
