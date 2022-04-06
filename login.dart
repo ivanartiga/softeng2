9195279c2ba1bd5b39e61d93edf70f5d24986875
ivanartiga <94870300+ivanartiga@users.noreply.github.com>
2022-04-06 12:36:22 +0800

    Update login.dart

--- login.dart
+++ login.dart
@@ -1,5 +1,8 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
-
+import 'package:flutter/services.dart';
+import 'package:provider/provider.dart';
+import 'package:fluttertoast/fluttertoast.dart';
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
 
@@ -8,22 +11,33 @@ class Login extends StatefulWidget {
 }
 
 class _LoginState extends State<Login> {
+  final _formKey = GlobalKey<FormState>();
+  final TextEditingController emailController = new TextEditingController();
+  final TextEditingController passwordController = new TextEditingController();
+
+  // firebase
+  final _auth = FirebaseAuth.instance;
+  // displaying error
+  String? errorMessage;
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       backgroundColor: Colors.white,
       appBar: AppBar(
-        title: Text("Barangay Basak Assistance System"),
+        title: Center(child: Text("Barangay Basak Assistance System")),
       ),
       body: SingleChildScrollView(
-        child: Column(
+        child: Form(
+          key: _formKey,
+          child:Column(
           children: <Widget>[
             Padding(
               padding: const EdgeInsets.all(50.0),
               child: Center(
                 child: Container(
-                    width: 200,
-                    height: 150,
+                    width: 300,
+                    height: 250,
                     /*decoration: BoxDecoration(
                         color: Colors.red,
                         borderRadius: BorderRadius.circular(50.0)),*/
@@ -32,39 +46,64 @@ class _LoginState extends State<Login> {
 
               ),
             ),
-            const Padding(
+             Padding(
               //padding: const EdgeInsets.only(left:15.0,right: 15.0,top:0,bottom: 0),
               padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
+              child: TextFormField(
+                autofocus: false,
+                controller: emailController,
+                keyboardType: TextInputType.emailAddress,
                 decoration: InputDecoration(
                     prefixIcon: Icon(Icons.account_circle),
                     border: OutlineInputBorder(),
-                    labelText: 'Username',
-                    hintText: 'Enter username'),
+                    labelText: 'Email',
+                    hintText: 'Enter Email'),
+                onSaved: (value){
+                  emailController.text=value!;
+                },
+                validator: (value)
+                {
+                if (value == null || value.isEmpty) {
+                return 'Can\'t be empty';
+                }
+                if (!RegExp("^[a-zA-Z0-9+_.-]+@[a-zA-Z0-9.-]+.[a-z]")
+                    .hasMatch(value)) {
+                  return ("Please Enter a valid email");
+                }
+                return null;
+                },
+                textInputAction: TextInputAction.next,
               ),
             ),
-            const Padding(
+            Padding(
               padding: EdgeInsets.only(
                   left: 15.0, right: 15.0, top: 15, bottom: 0),
               //padding: EdgeInsets.symmetric(horizontal: 15),
-              child: TextField(
+              child: TextFormField(
+                autofocus: false,
+                controller: passwordController,
                 obscureText: true,
                 decoration: InputDecoration(
                     prefixIcon: Icon(Icons.lock),
                     border: OutlineInputBorder(),
                     labelText: 'Password',
                     hintText: 'Enter secure password'),
+                onSaved: (value){
+                  passwordController.text=value!;
+                },
+                validator: (value) {
+                  RegExp regex = new RegExp(r'^.{6,}$');
+                  if (value!.isEmpty) {
+                    return ("Password is required for login");
+                  }
+                  if (!regex.hasMatch(value)) {
+                    return ("Enter Valid Password(Min. 6 Character)");
+                  }
+                },
+                textInputAction: TextInputAction.done,
               ),
             ),
-            TextButton(
-              onPressed: (){
-                //TODO FORGOT PASSWORD SCREEN GOES HERE
-              },
-              child: const Text(
-                'Forgot Password',
-                style: TextStyle(color: Colors.blue, fontSize: 15),
-              ),
-            ),
+            SizedBox(height:20),
             Container(
               height: 50,
               width: 250,
@@ -72,8 +111,7 @@ class _LoginState extends State<Login> {
                   color: Colors.blue, borderRadius: BorderRadius.circular(20)),
               child: TextButton(
                 onPressed: () {
-                  Navigator.pushNamed(context, '/userdashboard');
-
+                  signIn(emailController.text, passwordController.text);
                 },
                 child: Text(
                   'Login',
@@ -82,7 +120,7 @@ class _LoginState extends State<Login> {
               ),
             ),
             SizedBox(
-              height: 100,
+              height: 5,
             ),
             TextButton(
               onPressed: () {
@@ -91,12 +129,53 @@ class _LoginState extends State<Login> {
               },
               child: Text(
                 'Create an Account',
-                style: TextStyle(color: Colors.black, fontSize: 20),
+                style: TextStyle(color: Colors.grey, fontSize: 18),
               ),
             ),
+        //    Center(child: GoogleLoginButton()),
           ],
+        ),
         ),
       ),
     );
+  }
+  // login function
+  void signIn(String email, String password) async {
+    if (_formKey.currentState!.validate()) {
+      try {
+        await _auth
+            .signInWithEmailAndPassword(email: email, password: password)
+            .then((uid) => {
+          Fluttertoast.showToast(msg: "Login Successful"),
+          Navigator.pushNamed(context, '/userdashboard'),
+        });
+      } on FirebaseAuthException catch (error) {
+        switch (error.code) {
+          case "invalid-email":
+            errorMessage = "Your email address appears to be malformed.";
+
+            break;
+          case "wrong-password":
+            errorMessage = "Your password is wrong.";
+            break;
+          case "user-not-found":
+            errorMessage = "User with this email doesn't exist.";
+            break;
+          case "user-disabled":
+            errorMessage = "User with this email has been disabled.";
+            break;
+          case "too-many-requests":
+            errorMessage = "Too many requests";
+            break;
+          case "operation-not-allowed":
+            errorMessage = "Signing in with Email and Password is not enabled.";
+            break;
+          default:
+            errorMessage = "An undefined Error happened.";
+        }
+        Fluttertoast.showToast(msg: errorMessage!);
+        print(error.code);
+      }
+    }
   }
 }
