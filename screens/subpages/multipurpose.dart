28a46bcee4d7e35342ad0f9cdbf1e813d8e8bb75
ivanartiga <94870300+ivanartiga@users.noreply.github.com>
2022-04-06 13:10:42 +0800

    Create multipurpose.dart

---
+++ screens/subpages/multipurpose.dart
@@ -0,0 +1,16 @@
+import 'package:flutter/material.dart';
+import 'package:tuple/tuple.dart';
+
+class Multipurposereservation extends StatefulWidget {
+  const Multipurposereservation({Key? key}) : super(key: key);
+
+  @override
+  State<Multipurposereservation> createState() => _MultipurposereservationState();
+}
+
+class _MultipurposereservationState extends State<Multipurposereservation> {
+  @override
+  Widget build(BuildContext context) {
+    return Container(child: Text("MULTI PURPOSE RESERVATION"),);
+  }
+}
